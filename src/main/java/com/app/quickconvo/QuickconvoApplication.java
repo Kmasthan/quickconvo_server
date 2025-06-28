1db8055faeb85ebcffc598a1a4a8490d6581953b
Masthanaiah K <masthankalthireddy@gmail.com>
2025-06-28 13:39:21 +0530

    initial commit

---
+++ src/main/java/com/app/quickconvo/QuickconvoApplication.java
@@ -0,0 +1,13 @@
+package com.app.quickconvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickconvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickconvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/quickconvo/QuickconvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.quickconvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickconvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
